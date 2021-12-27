ecdf78769bf222833e1f2bd43630b8db8c86b6ce
Iztok Bajcar <ib8548@student.uni-lj.si>
2021-12-27 15:42:54 +0100

    Added some styles

--- web/index.php
+++ web/index.php
@@ -8,15 +8,40 @@
         <meta charset="UTF-8" />
         <title>Expenses calculator</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <style>
+            table {
+                border: 5px solid black;
+                margin-left: auto;
+                margin-right: auto;
+            }
+
+            th, td {
+                border: 1px solid black;
+                font-family: monospace;
+                padding: 8px;
+            }
+
+            th {
+                border-bottom: 2px solid black;
+                font-size: 20px;
+                text-align: center;
+            }
+        </style>
     </head>
     <body>
         Expenses calculator
         <?php 
             initDB(); 
             $expenses = getExpenses();
+            var_dump($expenses);
             if (count($expenses) == 0) {
                 // Array is empty
                 echo "<p>The database is currently empty.</p>";
+            } else {
+                if (count($expenses) > 20) {
+                    
+                }
+                echo createTableFromArray($expenses);
             }
         ?>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
